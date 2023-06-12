ed0401c3e32c7ec17f1a99cc0092e68bf2cdff7c
Tod Winchester <tod.winchester2@gmail.com>
2023-06-12 08:00:40 -0700

    Circle shape and text work fine, now I need to add Triangle and Square

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const inquirer = require("inquirer");
 const prompts = require("./Lib/prompts");
 const fs = require('fs');
 const Circle = require("./Lib/shapes");
+// const Triangle = require("./Lib/shapes");
+// const Square = require("./Lib/shapes");
 
 const init = async () => {
   try {
@@ -20,12 +22,36 @@ const init = async () => {
       // if the user selected circle, create a new object instance of your Circle class
       // you would do something similar for the other shapes--don't forget to import them up above
       shape = new Circle();
+      // shape = new Triangle();
+      // shape = new Square();
 
       // use the answers from the user to set shape properties
       shape.setColor(responses.shapeColor);
 
       output = shape.render(responses.text, responses.textColor);
     }
+    // if (responses.shape == "Triangle") {
+
+    //  if the user selected triangle, create a new object instance of your Triangle class
+    //you would do something similar for the other shapes--don't forget to import them up above
+   // shape = new Triangle();
+   
+    // use the answers from the user to set shape properties
+    //   shape.setColor(responses.shapeColor);
+
+    //   output = shape.render(responses.text, responses.textColor);
+    // }
+    // if (responses.shape == "Triangle") {
+
+    // if the user selected triangle, create a new object instance of your Triangle class
+    //  you would do something similar for the other shapes--don't forget to import them up above
+    //   // shape = new Triangle();
+
+    //   // use the answers from the user to set shape properties
+    //   shape.setColor(responses.shapeColor);
+
+    //   output = shape.render(responses.text, responses.textColor);
+    // }
     console.log("output", output);
 
     // write output to a file
@@ -44,54 +70,3 @@ init();
 // let output = shape.render();
 // console.log('output', output);
 
-// const generatemd = ({ title}) =>
-//   `# ${title}
-
-//   `;
-
-// inquirer
-//   .prompt([
-//     {
-//       type: 'list',
-//       name: 'text',
-//       message: 'Enter Character 1?',
-//       choices: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-//     },
-//     {
-//         type: 'list',
-//         name: 'text',
-//         message: 'Enter Character 2?',
-//         choices: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-//       },
-//       {
-//         type: 'list',
-//         name: 'text',
-//         message: 'Enter Character 3?',
-//         choices: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-//       },
-//     {
-//       type: 'list',
-//       name: 'textColor',
-//       message: 'Please choose your text Color',
-//       choices: ["Red", "Orange", "Yellow", "Green", "Blue", "Violet"]
-//     },
-//     {
-//       type: 'list',
-//       name: 'shape',
-//       message: 'Please choose your outline shape?',
-//       choices: ["Circle", "Triangle", "Square"]
-//     },
-//     {
-//       type: 'list',
-//       name: 'shapeColor',
-//       message: 'Please choose your shape color?',
-//       choices: ["Red", "Orange", "Yellow", "Green", "Blue", "Violet"]
-//     },
-//   ])
-//   .then((answers) => {
-//     const markdownPageContent = generatemd(answers);
-
-//     fs.writeFileSync('index.md', generatemd(answers), (err) =>
-//       err ? console.log(err) : console.log('Successfully generated logo.svg!')
-//     );
-//   });
